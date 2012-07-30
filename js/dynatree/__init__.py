f785284ba01f998b100f469c9b2b1e33b0c01a43
jdelic <jdelic@d598d175-8310-0410-ada9-90c0a3372496>
2012-07-30 15:40:33 +0000

    add: added css

--- js/dynatree/__init__.py
+++ js/dynatree/__init__.py
@@ -4,6 +4,7 @@ import js.jqueryui
 
 library = Library('dynatree', 'resources')
 
+dynatree_css = Resource(library, 'src/skin/ui.dynatree.css')
 dynatree = Resource(library, 'src/jquery.dynatree.js',
         minified='src/jquery.dynatree.min.js',
-        depends=[js.jquery.jquery, js.jqueryui.jqueryui])
+        depends=[dynatree_css, js.jquery.jquery, js.jqueryui.jqueryui])
