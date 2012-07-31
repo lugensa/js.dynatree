d193d11b9ea3c1c022deefc0a29f603626de9132
jdelic <jdelic@d598d175-8310-0410-ada9-90c0a3372496>
2012-07-31 15:40:16 +0000

    mod: use by default vista icons

--- js/dynatree/__init__.py
+++ js/dynatree/__init__.py
@@ -4,7 +4,7 @@ import js.jqueryui
 
 library = Library('dynatree', 'resources')
 
-dynatree_css = Resource(library, 'src/skin/ui.dynatree.css')
+dynatree_css = Resource(library, 'src/skin-vista/ui.dynatree.css')
 dynatree = Resource(library, 'src/jquery.dynatree.js',
         minified='src/jquery.dynatree.min.js',
         depends=[dynatree_css, js.jquery.jquery, js.jqueryui.jqueryui])
